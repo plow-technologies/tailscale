289a90790b9767fd30d7fc99ad2dd5d190dae7a3
Scott Murphy <scottmurphy09@gmail.com>
2022-02-06 20:19:40 -0600

    upcase

--- net/interfaces/interfaces_test.go
+++ net/interfaces/interfaces_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestGetState(t *testing.T) {
-        t.skip("test fails for Plow Build")
+        t.Skip("test fails for Plow Build")
 	st, err := GetState()
 	if err != nil {
 		t.Fatal(err)
